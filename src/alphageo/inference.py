5383de5d45af644707030a74ca96fc120fdbc7f5
Tianxiang Xia <xia.tianxiang@huawei.com>
2024-07-24 14:01:42 +0200

    Hyperparameter controlling

--- src/alphageo/inference.py
+++ src/alphageo/inference.py
@@ -6,7 +6,9 @@ from torch.nn.functional import pad
 from alphageo.model import Decoder
 
 
-def brevity_penalty(length : int, alpha : float = 0.6, numerator_bias : int = 5, denominator_bias : int = 6) -> float:
+def brevity_penalty(
+    length: int, alpha: float = 0.6, numerator_bias: int = 5, denominator_bias: int = 6
+) -> float:
     """
     Calculate a brevity penalty for short sequences.
     In Language Modeling, short sequences are usually inherently more likley than longer ones because of the overall probability of a sequence is the product of probabilities for each word P(w1...wn) = P(wn) * P(wn-1) * ... * P(w1).
@@ -25,7 +27,13 @@ def brevity_penalty(length : int, alpha : float = 0.6, numerator_bias : int = 5,
 
 
 def priority_beam_search(
-    model : Decoder, inp : Tensor, beam_width : int = 4, num_return_sequences : int = 2, eos_id : int = 263, max_new_tokens : int = 512, tokenizer : Any = None
+    model: Decoder,
+    inp: Tensor,
+    beam_width: int = 4,
+    num_return_sequences: int = 2,
+    eos_id: int = 263,
+    max_new_tokens: int = 16,
+    tokenizer: Any = None,
 ) -> list[tuple[Tensor, float]]:
     """
     Beam search with a priority queue, designed to be close to AG's beam search at
@@ -44,10 +52,10 @@ def priority_beam_search(
     Returns:
         A list of `[(seq1, score1), ..., (seq_n, score_n)]` tuples of sequence token IDs (list of int) and final sequence log prob (float).
     """
-    live_sequences : list[tuple[Tensor, float]] = [
+    live_sequences: list[tuple[Tensor, float]] = [
         (inp, 0.0)
     ]  # essentially a priority queue for unfinished sequences
-    finished_sequences : list[tuple[Tensor, float]] = []
+    finished_sequences: list[tuple[Tensor, float]] = []
     start_len = inp.shape[-1]
     batch_size = beam_width  # at each time step, we expand the top beam_width sequences
 
@@ -62,7 +70,8 @@ def priority_beam_search(
         # break condition:
         if (
             # we have at least as many finished sequences as we want to return
-            finished_sequences and len(finished_sequences) >= num_return_sequences
+            finished_sequences
+            and len(finished_sequences) >= num_return_sequences
             # the current best unfinished sequence is *worse* than the worst finished one.
             and cur_batch[0][1] < finished_sequences[-1][1]
         ):
@@ -77,7 +86,7 @@ def priority_beam_search(
             [pad(item[0], (0, max_len - item[0].shape[-1])) for item in cur_batch]
         )
 
-        batch_log_p : list[Tensor] = model(batch_inp).log_softmax(dim=-1)
+        batch_log_p: list[Tensor] = model(batch_inp).log_softmax(dim=-1)
 
         for b_idx, (log_p, length) in enumerate(zip(batch_log_p, batch_lens)):
             cur_inp, cur_score = cur_batch[b_idx]  # input Tensor, current score
@@ -123,7 +132,12 @@ def priority_beam_search(
 
 
 def simple_beam_search(
-    model : Decoder, inp : Tensor, beam_width : int =4, num_return_sequences : int =2, eos_idx : int =263, max_tokens : int=128
+    model: Decoder,
+    inp: Tensor,
+    beam_width: int = 4,
+    num_return_sequences: int = 2,
+    eos_idx: int = 263,
+    max_tokens: int = 128,
 ) -> list[tuple[Tensor, float]]:
     """
     Implementation of a very straight-forward beam search. A bit simpler than priority_beam_search, but not guranteed to generate the same sequences (though it often may).
