840ffba50fd2c5f18d072538fa345b3c794e7f16
Vladmir Sicca <vsiccag@gmail.com>
2024-09-16 17:36:35 +0200

    Creation of a minimal set of problems to exemplify the LLM choices for the paper.

---
+++ llm_examplification.py
@@ -0,0 +1,13 @@
+import os
+
+need_aux = [
+    "translated_imo_2012_p5",
+    "translated_usamo_1997_p2",
+]
+
+
+if __name__ == "__main__":
+    for problem in need_aux:
+        os.system(
+            f"alphageo --device cpu -o None --problems-file problems_datasets/llm_examplification.txt --problem {problem} --search-width 32 --search-depth 3 --batch-size 32 --lm-beam-width 32"
+        )
