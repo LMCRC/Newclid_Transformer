aa65e3eaf18939c5143e77347728f357ed997fee
Vladmir Sicca <vsiccag@gmail.com>
2024-09-17 09:22:31 +0200

    🚧 Second attempt of rearranging the scripts for the run of the examples of the LLM on ROMA.

--- llm_examplification.py
+++ llm_examplification.py
@@ -9,5 +9,5 @@ need_aux = [
 if __name__ == "__main__":
     for problem in need_aux:
         os.system(
-            f"alphageo --device cpu -o None --problems-file problems_datasets/llm_examplification.txt --problem {problem} --search-width 32 --search-depth 3 --batch-size 32 --lm-beam-width 32"
+            f"alphageo --device cpu -o None --problems-file problems_datasets/llm_examplification.txt --problem {problem} --search-width 32 --search-depth 3 --batch-size 32 --lm-beam-width 32 --log-level 30"
         )
