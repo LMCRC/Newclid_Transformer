df4fb68728d49faf96420fe4348c9dca6562fcac
Vladmir Sicca <vsiccag@gmail.com>
2024-11-06 13:19:21 +0100

    Preparation of ROMA run of the IMO 2008 P1.

--- llm_examplification.py
+++ llm_examplification.py
@@ -1,13 +1,12 @@
 import os
 
 need_aux = [
-    "translated_imo_2012_p5",
-    "translated_usamo_1997_p2",
+    "translated_imo_2008_p1_all_extras",
 ]
 
 
 if __name__ == "__main__":
     for problem in need_aux:
         os.system(
-            f"alphageo --device cpu -o None --problems-file problems_datasets/llm_examplification.txt --problem {problem} --search-width 32 --search-depth 3 --batch-size 32 --lm-beam-width 32 --log-level 30"
+            f"alphageo --device cpu -o exp --problems-file problems_datasets/llm_examplification.txt --problem {problem} --search-width 32 --search-depth 3 --batch-size 32 --lm-beam-width 32 --log-level 20"
         )
